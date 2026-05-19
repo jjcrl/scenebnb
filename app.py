0298c29e3de3dceeea1b9718fefcca60cf9a694f
jjcrl <j412cff@icloud.com>
2026-05-19 12:01:19 +0100

    methods for create new space and get space by id complete.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from lib.database_connection import get_flask_database_connection
 from lib.database_connection import DatabaseConnection
 from lib.user_repo import UserRepo
 from lib.user import User
+from lib.space_repository import SpaceRepository
 
 is_test_mode = (os.getenv('APP_ENV') == 'test')
 connection = DatabaseConnection(is_test_mode)
@@ -14,20 +15,17 @@ app = Flask(__name__)
 
 # == Your Routes Here ==
 
-# GET /index
-# Returns the homepage
-# Try it:
-#   ; open http://localhost:5001/index
+# GET /index -> homepage
 @app.route("/index", methods=["GET"])
 def get_index():
     return render_template("index.html")
 
+#GET /users/new -> form for signup
 @app.route("/users/new", methods=["GET"])
 def get_signup_form():
     return render_template("signup_form.html")
 
-
-# POST /users
+# POST /users -> create new user
 @app.route("/users", methods=["POST"])
 def create_user():
     user_repo = UserRepo(connection)
@@ -39,6 +37,14 @@ def create_user():
     )
     user_repo.create(new_user)
     return redirect("/index")
+
+#GET /spaces -> get all spaces
+@app.route("/spaces", methods=["GET"])
+def gel_all_spaces():
+    spaces_repo = SpaceRepository(connection)
+    spaces = spaces_repo.all()
+    return render_template("spaces.html",spaces=spaces)
+
 
 
 # These lines start the server if you run this file directly
--- tests/test_space_repository.py
+++ tests/test_space_repository.py
@@ -3,14 +3,29 @@ from lib.database_connection import *
 """
 List all spaces
 """
-def test_get_all_spaces():
-    connection = DatabaseConnection()
-    connection.connect()
-    space_repository = SpaceRepository(connection)
+def test_get_all_spaces(db_connection):
+    db_connection.seed("seeds/setup_seeds.sql")
+    space_repository = SpaceRepository(db_connection)
     spaces = space_repository.all()
     assert spaces == [Space(1, 'Beach House', 'Brighton', 'The Crown', 120.00, 'A lovely place', True, 1),
                 Space(2, 'City Flat', 'London', 'Sherlock', 85.00, 'Central location', False, 1)]
 
 """
-list all available spaces?
+list space by id
 """
+def test_get_space_by_id(db_connection):
+    db_connection.seed("seeds/setup_seeds.sql")
+    space_repository = SpaceRepository(db_connection)
+    space = space_repository.find_by_id(1)
+    assert space == Space(1, 'Beach House', 'Brighton', 'The Crown', 120.00, 'A lovely place', True, 1)
+
+"""
+create a new space
+"""
+def test_create_space(db_connection):
+    db_connection.seed("seeds/setup_seeds.sql")
+    space_repository = SpaceRepository(db_connection)
+    new_space = Space(None,'New Space', 'Manchester', 'Coronation Street', 95.00, 'A great place', True, user_id=1)
+    space_repository.create(new_space)
+    spaces = space_repository.all()
+    assert len(spaces) == 3
