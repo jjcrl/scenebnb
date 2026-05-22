6ca672c127e5d00461090e45f07cb75d7a6e6748
jjcrl <j412cff@icloud.com>
2026-05-22 12:39:13 +0100

    added two new spaces

--- seeds/prod_seeds.sql
+++ seeds/prod_seeds.sql
@@ -34,7 +34,8 @@ CREATE TABLE bookings (
     status text
 );
 
-INSERT INTO users (name, email_address, password) VALUES ('Joe Bloggs', 'joe@bloggs.com', 'Password1234'),
+INSERT INTO users (name, email_address, password) VALUES 
+('Joe Bloggs', 'joe@bloggs.com', 'Password1234'),
 ('Ola Fagbenro', 'ola@fagbenro.com', 'Ola1234'),
 ('Zoe Jackson', 'zoe@jackson.com', 'Zoe1234'),
 ('Zein Raf', 'zein@rafi.com', 'Zein1234'),
@@ -44,7 +45,14 @@ INSERT INTO users (name, email_address, password) VALUES ('Joe Bloggs', 'joe@blo
 INSERT INTO spaces (title, location, tv_show, price, description, available_from, available_to, image_url, user_id)
 VALUES
 ('Beach House', 'Brighton', 'The Crown', 120.00, 'A lovely place', '2026-01-01', '2026-12-31', 'https://pictures.escapia.com/SURFSC/181095/9175490499.jpg', 1),
-('City Flat', 'London', 'Sherlock', 85.00, 'Central location', '2026-01-01', '2026-12-31', 'https://media.houseandgarden.co.uk/photos/618938787ec4df9dbbfebc7f/master/w_1600%2Cc_limit/8fb319cfcc817fa00eaee66e368db0cb-house-11jan17-Arwel-Wyn-Jones--BBC_b.jpg', 1);
+('City Flat', 'London', 'Sherlock', 85.00, 'Central location', '2026-01-01', '2026-12-31', 'https://media.houseandgarden.co.uk/photos/618938787ec4df9dbbfebc7f/master/w_1600%2Cc_limit/8fb319cfcc817fa00eaee66e368db0cb-house-11jan17-Arwel-Wyn-Jones--BBC_b.jpg', 1),
+('The Soprano Family Home', 'New Jersey', 'The Sopranos', 500.00, 'A Sprawling family estate, with a pool, large entertaining space, and sereval large bedrooms, definitely not bugged...', '2026-01-01', '2026-12-31', 'https://static.wikia.nocookie.net/sopranos/images/2/2f/Sopranos-House.jpeg/revision/latest?cb=20220103175642', 1),
+('The Cottage', 'Canada', 'Heated Rivalry', 250.00, 'A modern cottage over looking a beautiful lake, perfect for closested hockey players, or any lovers looking for a secret escape.', '2026-01-01', '2026-12-31', 'https://cdn.mos.cms.futurecdn.net/kzKfjqjYF758D9sSi3gUHH.jpg', 1);
+
+
+
+
+
 
 INSERT INTO bookings (space_id, user_id, date, status) VALUES (1, 1, '2026-05-20', 'pending');
 
